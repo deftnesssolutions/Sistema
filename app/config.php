a1c71f80632d71e1e4d3cb3a50e9c2ad7b515c42
Gustavo Ovelar <gusove@gmail.com>
2017-07-29 13:34:13 -0300

    Configuración del HXPHP Framework

--- app/config.php
+++ app/config.php
@@ -4,6 +4,15 @@ $configs = new HXPHP\System\Configs\Config;
 ActiveRecord\Connection::$datetime_format = 'Y-m-d H:i:s';
 
 $configs->env->add('development');
+$configs->env->development->baseURI = '/sistema/';
+$configs->env->development->database->setConnectionData(array(
+  'driver' => 'mysql',
+  'host' => 'localhost',
+  'user' => 'root',
+  'password' => 'admin',
+  'dbname' => 'tarefatodolit',
+  'charset' => 'utf8'
+  ));
 
 
 /*
@@ -13,7 +22,7 @@ $configs->env->add('development');
   //Configurações de Ambiente - Desenvolvimento
   $configs->env->add('development');
 
-  $configs->env->development->baseURI = '/hxphp/';
+  $configs->env->development->baseURI = '/sistema/';
 
   $configs->env->development->database->setConnectionData([
   'driver' => 'mysql',
