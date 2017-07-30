5212ecd25e0620baef799c8ebde354d5c07e3ea9
Gustavo Ovelar <gusove@gmail.com>
2017-07-30 18:17:11 -0300

    Tratamiento de Valores unicos con validates_iniqueness_of y validates_presence_of

--- app/models/User.php
+++ app/models/User.php
@@ -2,9 +2,39 @@
 
 class User extends \HXPHP\System\Model
 {
+	static $validates_presence_of = array(
+      array(
+      	'name',
+      	'message' => 'O nome é um campo obrigatorio.'
+      	),
+      array(
+      	'email',
+      	'message' => 'O e-mail é um campo obrigatorio.'
+      	),
+      array(
+      	'username',
+      	'message' => 'O nome de usuario é um campo obrigatorio.'
+      	),
+      array(
+      	'password',
+      	'message' => 'A senha é um campo obrigatorio.'
+      	)
+    );
+
+	static $validates_uniqueness_of = array(
+       array(
+       		array('username', 'email'),
+       		'message' => 'Ja existe um usuario com este e-mail e/ou nome de usuario cadastrado.'
+       	)
+    );
 	public static function cadastrar(array $post)
 	{
 		$role = Role::find_by_role('user');
+
+		if(is_null($role))
+		{
+			return false;
+		}
 		
 		$post = array_merge($post,array(
 			'role_id'=> $role->id,
@@ -15,7 +45,9 @@ class User extends \HXPHP\System\Model
 
 		$post = array_merge($post,$password);
 
-		return self::create($post);
+		$cadastrar= self::create($post);
+
+		var_dump($cadastrar->errors->get_raw_errors());
 
 	}
 }
