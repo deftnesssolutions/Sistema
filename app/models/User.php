6b1bf9d5997f08389675677f3dad4dbf1d096e6b
Gustavo Ovelar <gusove@gmail.com>
2017-08-01 11:47:51 -0300

    Corrección de la validación de exclusividad en el cadastro de usuario

--- app/models/User.php
+++ app/models/User.php
@@ -23,9 +23,13 @@ class User extends \HXPHP\System\Model
 
 	static $validates_uniqueness_of = array(
        array(
-       		array('username', 'email'),
-       		'message' => 'Ja existe um usuario com este e-mail e/ou nome de usuario cadastrado.'
-       	)
+			'username',
+			'message' => 'Já existe um usuário com este nome de usuário cadastrado.'
+		),
+		array(
+			'email',
+			'message' => 'Já existe um usuário com este e-mail cadastrado.'
+		)
     );
 	public static function cadastrar(array $post)
 	{
